d7a1ad1dfb287dd54f7be6a94a6e38aa9168e97a
Chen Yu Pao <windperson@gmail.com>
2014-06-30 15:33:40 +0800

    workable example

---
+++ TestJSP/WebContent/hello.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF8"
+    pageEncoding="UTF8"%>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF8">
+<title>${param.user}</title>
+</head>
+<body>
+	<h1>${message}</h1>
+</body>
+</html>
--- TestJSP/src/com/binodata/Hello.java
+++ TestJSP/src/com/binodata/Hello.java
@@ -1,12 +1,22 @@
 package com.binodata;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Hello {
 	
+	private Map<String, String> messages;
 	
+	public Hello(){
+		messages = new HashMap<String, String>();
+		messages.put("caterpillar", "Hello");
+		messages.put("Justin", "Welcome");
+		messages.put("momor", "Hi");
+	}
 
-	public String doHello(String name) {
-		
-		return null;
+	public String doHello(String user) {
+		String message = messages.get(user);
+		return message + ", " + user + "!";
 	}
 
 }
